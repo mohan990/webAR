0999fffc0c3c70cd68db00d09ec437d9f3a44789
mohan990 <mohanprabhueie@gmail.com>
2022-10-20 14:45:48 +0530

    updated camera position

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    var entity = document.querySelector('#mario');
+    // entity.setAttribute('light', {
+    //     type: 'spot',
+    //     distance: 30,
+    //     intensity: 2.0
+    //   });
+    //   console.log('entity',entity)
+}
