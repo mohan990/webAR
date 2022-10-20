5e9fa49ab6f2f73f03d696db65310b8cb5336165
mohan990 <mohanprabhueie@gmail.com>
2022-10-20 14:54:13 +0530

    changes made

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    var entity = document.querySelector('#mario');
-    // entity.setAttribute('light', {
-    //     type: 'spot',
-    //     distance: 30,
-    //     intensity: 2.0
-    //   });
-    //   console.log('entity',entity)
-}
